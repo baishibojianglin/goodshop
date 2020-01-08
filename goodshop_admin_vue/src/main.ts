a698c691eb8f3d7c027464d85f489d8c54497ba1
Greetme <1980900198@qq.com>
2020-01-08 19:13:17 +0800

    yan: region

--- goodshop_admin_vue/src/main.ts
+++ goodshop_admin_vue/src/main.ts
@@ -21,7 +21,7 @@ Vue.prototype.$sign = 'jl_goodshop';
 //引入element-ui
 import 'element-ui/lib/theme-chalk/index.css';
 import 'element-ui/lib/theme-chalk/base.css';
-import { Row,Col,Button,Input,Message,Form,FormItem} from 'element-ui';
+import { Row,Col,Button,Input,Message,Form,FormItem,Card,Popconfirm} from 'element-ui';
 import CollapseTransition from 'element-ui/lib/transitions/collapse-transition';
 
 
@@ -33,6 +33,8 @@ Vue.use(Row);
 Vue.use(Col);
 Vue.use(Form);
 Vue.use(FormItem);
+Vue.use(Card);
+Vue.use(Popconfirm);
 Vue.component(CollapseTransition.name, CollapseTransition);
 
 
--- goodshop_admin_vue/src/pages/region/Region.vue
+++ goodshop_admin_vue/src/pages/region/Region.vue
@@ -1,5 +1,65 @@
 <template>
-	<div class="region">Region</div>
+	<div class="region">
+		<el-card class="box-card">
+			<el-row :gutter="12">
+				<el-col :xs="12" :sm="8" :md="8" :lg="6" :xl="1" style="margin-bottom: 1rem;">
+					<el-card shadow="hover">
+					<span>北京市</span>
+					<div style="margin-top: 1rem;">
+						<el-button type="primary" size="mini" plain icon="el-icon-edit">管理</el-button>
+						<el-popconfirm confirmButtonText='确定' cancelButtonText='取消' icon="el-icon-info" iconColor="red" title="确定删除该区域？" style="margin-left: 0.5rem;">
+							<el-button type="danger" size="mini" plain icon="el-icon-delete" slot="reference">删除</el-button>
+						</el-popconfirm>
+					</div>
+					</el-card>
+				</el-col>
+				<el-col :xs="12" :sm="8" :md="8" :lg="6" :xl="1" style="margin-bottom: 1rem;">
+					<el-card shadow="hover">
+					<span>天津市</span>
+					<div style="margin-top: 1rem;">
+						<el-button type="primary" size="mini" plain icon="el-icon-edit">管理</el-button>
+						<el-popconfirm confirmButtonText='确定' cancelButtonText='取消' icon="el-icon-info" iconColor="red" title="确定删除该区域？" style="margin-left: 0.5rem;">
+							<el-button type="danger" size="mini" plain icon="el-icon-delete" slot="reference">删除</el-button>
+						</el-popconfirm>
+					</div>
+					</el-card>
+				</el-col>
+				<el-col :xs="12" :sm="8" :md="8" :lg="6" :xl="1" style="margin-bottom: 1rem;">
+					<el-card shadow="hover">
+					<span>上海市</span>
+					<div style="margin-top: 1rem;">
+						<el-button type="primary" size="mini" plain icon="el-icon-edit">管理</el-button>
+						<el-popconfirm confirmButtonText='确定' cancelButtonText='取消' icon="el-icon-info" iconColor="red" title="确定删除该区域？" style="margin-left: 0.5rem;">
+							<el-button type="danger" size="mini" plain icon="el-icon-delete" slot="reference">删除</el-button>
+						</el-popconfirm>
+					</div>
+					</el-card>
+				</el-col>
+				<el-col :xs="12" :sm="8" :md="8" :lg="6" :xl="1" style="margin-bottom: 1rem;">
+					<el-card shadow="hover">
+					<span>重庆市</span>
+					<div style="margin-top: 1rem;">
+						<el-button type="primary" size="mini" plain icon="el-icon-edit">管理</el-button>
+						<el-popconfirm confirmButtonText='确定' cancelButtonText='取消' icon="el-icon-info" iconColor="red" title="确定删除该区域？" style="margin-left: 0.5rem;">
+							<el-button type="danger" size="mini" plain icon="el-icon-delete" slot="reference">删除</el-button>
+						</el-popconfirm>
+					</div>
+					</el-card>
+				</el-col>
+				<el-col :xs="12" :sm="8" :md="8" :lg="6" :xl="1" style="margin-bottom: 1rem;">
+					<el-card shadow="hover">
+					<span>四川省</span>
+					<div style="margin-top: 1rem;">
+						<el-button type="primary" size="mini" plain icon="el-icon-edit">管理</el-button>
+						<el-popconfirm confirmButtonText='确定' cancelButtonText='取消' icon="el-icon-info" iconColor="red" title="确定删除该区域？" style="margin-left: 0.5rem;">
+							<el-button type="danger" size="mini" plain icon="el-icon-delete" slot="reference">删除</el-button>
+						</el-popconfirm>
+					</div>
+					</el-card>
+				</el-col>
+			</el-row>
+		</el-card>
+	</div>
 </template>
 
 <script>
