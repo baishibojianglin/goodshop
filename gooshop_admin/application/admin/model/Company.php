c9f115655b8da66e4caad79aa716e5846273db0e
Greetme <1980900198@qq.com>
2020-02-10 18:49:57 +0800

    company

--- gooshop_admin/application/admin/model/Company.php
+++ gooshop_admin/application/admin/model/Company.php
@@ -17,24 +17,27 @@ class Company extends Base
 
     /**
      * 创建供应商
+<<<<<<< HEAD
+=======
      * @param {object}
      * @return array|false|\PDOStatement|string|Model
+>>>>>>> 66cd755b4c00ce39fd848d541829a689a6d9a694
      */
     public function inCompany($data)
     {
         //入库供应商基本信息表
         $this->save([
-            'name' => $data['name'],
-            'address' => $data['address'],
-            'role' => 1,
-            'upid' => $data['upid'],
-            'status' =>  1,
-            'type' => 1,
-            'license_creditcode' => $data['license_creditcode'],
-            'url_license' => $data['url_license'],
-            'legalperson_name' => $data['legalperson_name'],
-            'url_idcard' => $data['url_idcard'],
-            'legalperson_idcard_code' => $data['legalperson_idcard_code']
+            'name'= $data['name'];
+            'address' = $data['address'];
+            'role' = 1;
+            'upid' =  $data['upid'];
+            'status' =  1;
+            'type' =  1;
+            'license_creditcode' =  $data['license_creditcode'];
+            'url_license' =  $data['url_license'];
+            'legalperson_name' =  $data['legalperson_name'];
+            'url_idcard' =  $data['url_idcard'];
+            'legalperson_idcard_code' =  $data['legalperson_idcard_code'];
         ]);
 
 
