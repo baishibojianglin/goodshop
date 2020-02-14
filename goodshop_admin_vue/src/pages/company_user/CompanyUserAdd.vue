b778e5c2de9eabeb8fd43ddac0ed20d7297ad163
Greetme <1980900198@qq.com>
2020-02-14 10:26:32 +0800

    company_user

--- goodshop_admin_vue/src/pages/company_user/CompanyUserAdd.vue
+++ goodshop_admin_vue/src/pages/company_user/CompanyUserAdd.vue
@@ -14,7 +14,7 @@
 				<el-form ref="ruleForm" :model="form" :rules="rules" label-width="100px" size="small" class="demo-form-inline">
 					<el-form-item label="供应商" prop="company_id">
 						<el-select v-model="form.company_id" :disabled="companySelectDisabled" placeholder="请选择…" filterable>
-							<el-option :key="0" label="平台" :value="0"></el-option>
+							<el-option :key="0" label="供应商联盟平台" :value="0"></el-option>
 							<el-option
 								v-for="item in companyOptions"
 								:key="item.id"
@@ -35,11 +35,30 @@
 					<el-form-item prop="account" label="供应商账户号">
 						<el-input v-model="form.account" placeholder="输入供应商账户号" clearable style="width:350px;"></el-input>
 					</el-form-item>
+					<el-form-item prop="password" label="登录密码">
+						<el-input v-model="form.password" placeholder="请输入密码" clearable show-password style="width:350px;"></el-input>
+					</el-form-item>
 					<el-form-item prop="phone" label="电话号码">
 						<el-input v-model="form.phone" placeholder="输入供应商账户电话号码" clearable style="width:350px;"></el-input>
 					</el-form-item>
 					<el-form-item prop="ratio" label="提成比例">
 						<el-input v-model="form.ratio" placeholder="输入供应商账户提成比例" clearable style="width:350px;"></el-input>
+					</el-form-item>
+					<el-form-item prop="status" label="状态">
+						<el-radio-group v-model="form.status">
+							<el-radio :label="1">启用</el-radio>
+							<el-radio :label="0">禁用</el-radio>
+						</el-radio-group>
+					</el-form-item>
+					<el-form-item label="角色" prop="group_id">
+						<el-select v-model="form.group_id" placeholder="请选择…" filterable>
+							<el-option
+								v-for="item in authGroupOptions"
+								:key="item.id"
+								:label="item.title"
+								:value="item.id">
+							</el-option>
+						</el-select>
 					</el-form-item>
 					<el-form-item>
 						<el-button type="primary" plain @click="submitForm('ruleForm')">提交</el-button>
@@ -61,28 +80,46 @@
 					user_name: '', // 供应商账户名称
 					avatar: '', // 供应商账户证件照
 					account: '', // 供应商账户号
+					password: '', // 密码
 					phone: '', // 电话号码
 					ratio: '', // 提成比例
+					status: '', // 状态 
+					group_id: '', // 角色ID
 				},
 				rules: { // 验证规则
+					company_id: [
+						{ required: true, message: '请选择供应商', trigger: 'change' }
+					],
 					user_name: [
 						{ required: true, message: '请输入供应商账户名称', trigger: 'blur' },
 						{ min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }
 					],
+					/* avatar: [
+						{ required: true, message: '请上传供应商账户证件照', trigger: 'blur' }
+					], */
 					account: [
 						{ required: true, message: '请输入供应商账户号', trigger: 'blur' },
 						{ min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }
 					],
-					/* avatar: [
-						{ required: true, message: '请上传供应商账户证件照', trigger: 'blur' }
-					] */
+					password: [
+						{ required: true, message: '请输入密码', trigger: 'blur' },
+						{ min: 6, max: 20, message: '长度在 6 到 20 个字符', trigger: 'blur' }
+					],
+					phone: [
+						{ required: true, pattern: /^1[34578]\d{9}$/, message: '目前只支持中国大陆的手机号码', trigger: 'blur' }
+					],
+					group_id: [
+						{ required: true, message: '请选择角色', trigger: 'change' }
+					],
 				},
 				companyOptions: [], // 供应商下拉框列表
 				companySelectDisabled: false, // 供应商下拉框禁用状态
+				authGroupOptions: [], // 角色下拉框列表
 			}
 		},
 		created() {
 			this.getCompanyTree(); // 获取供应商列表树
+			this.getAuthGroupTree(); // 获取角色列表树
 			
 			// 供应商ID
 			this.form.company_id = JSON.parse(localStorage.getItem('company')).company_id;
@@ -116,6 +153,36 @@
 			},
 			
 			/**
+			 * 获取角色列表树
+			 */
+			getAuthGroupTree() {
+				let self = this;
+				this.$axios.get(this.$url + 'auth_group_tree', {
+					// 请求头配置
+					headers: {
+						'company-user-id': JSON.parse(localStorage.getItem('company')).user_id,
+						'company-user-token': JSON.parse(localStorage.getItem('company')).token
+					}
+				})
+				.then(function(res) {
+					if (res.data.status == 1) {
+						self.authGroupOptions = res.data.data;
+					} else {
+						self.$message({
+							message: '网络忙，请重试',
+							type: 'warning'
+						});
+					}
+				})
+				.catch(function (error) {
+					self.$message({
+						message: error.response.data.message,
+						type: 'warning'
+					});
+				});
+			},
+			
+			/**
 			 * 新增供应商账户提交表单
 			 * @param {Object} formName
 			 */
@@ -129,8 +196,11 @@
 							user_name: this.form.user_name,
 							avatar: this.form.avatar,
 							account: this.form.account,
+							password: this.form.password,
 							phone: this.form.phone,
-							ratio: this.form.ratio
+							ratio: this.form.ratio,
+							status: this.form.status,
+							group_id: this.form.group_id
 						}, {
 							// 请求头配置
 							headers: {
--- gooshop_admin/application/admin/controller/CompanyUser.php
+++ gooshop_admin/application/admin/controller/CompanyUser.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 
 use app\common\lib\exception\ApiException;
 use think\Controller;
+use think\Db;
 use think\Request;
 
 /**
@@ -95,23 +96,36 @@ class CompanyUser extends Base
                 $data['parent_id'] = $this->companyUser['user_id']; // TODO：parent_id待处理
             }
 
+            $data['password'] = md5($data['password']); // 密码
             $data['status'] = isset($data['status']) ? $data['status'] : config('code.status_disable'); // 状态
             $data['create_time'] = time(); // 创建时间
             $data['create_ip'] = request()->ip(); // 创建IP
 
-            // 新增
-            // 捕获异常
+            /* 手动控制事务 s */
+            // 启动事务
+            Db::startTrans();
             try {
-                $id = model('CompanyUser')->insertGetId($data); // 新增数据并返回主键值
+                // 新增供应商账户
+                $res[0] = $userId = Db::name('company_user')->strict(false)->insertGetId($data); // 新增数据并返回主键值
+
+                // 绑定供应商账户角色
+                $data1 = ['uid' => $userId, 'group_id' => $data['group_id']];
+                $res[1] = Db::name('auth_group_access')->insert($data1);
+
+                // 任意一个表写入失败都会抛出异常，TODO：是否可以不做该判断
+                if (in_array(0, $res)) {
+                    return show(config('code.error'), '供应商账户新增失败', '', 403);
+                }
+
+                // 提交事务
+                Db::commit();
+                return show(config('code.success'), '供应商账户新增成功', '', 201);
             } catch (\Exception $e) {
+                // 回滚事务
+                Db::rollback();
                 return show(config('code.error'), '网络忙，请重试', '', 500);
             }
-            // 判断是否新增成功：获取id
-            if ($id) {
-                return show(config('code.success'), '供应商账户新增成功', '', 201);
-            } else {
-                return show(config('code.error'), '供应商账户新增失败', '', 403);
-            }
+            /* 手动控制事务 e */
         } else {
             return show(config('code.error'), '请求不合法', '', 400);
         }
@@ -128,7 +142,7 @@ class CompanyUser extends Base
         // 判断为GET请求
         if (request()->isGet()) {
             try {
-                $data = model('CompanyUser')->find($id);
+                $data = model('CompanyUser')->alias('cu')->field('cu.user_id, company_id, cu.user_name, cu.avatar, cu.account, cu.phone, cu.ratio, cu.status, aga.group_id')->join('__AUTH_GROUP_ACCESS__ aga', 'cu.user_id = aga.uid', 'LEFT')->find($id);
             } catch (\Exception $e) {
                 return show(config('code.error'), '网络忙，请重试', '', 500);
             }
@@ -192,6 +206,9 @@ class CompanyUser extends Base
                 return show(config('code.error'), '供应商账户号已存在', '', 403);
             }
         }
+        if (!empty($param['password'])) { // 密码
+            $data['password'] = md5($param['password']);
+        }
         if (!empty($param['phone'])) { // 电话号码
             $data['phone'] = trim($param['phone']);
 
@@ -212,22 +229,52 @@ class CompanyUser extends Base
                 return show(config('code.error'), '供应商账户已登录，不能禁用', '', 403);
             }
         }
+        if (!empty($param['company_id'])) { // 供应商ID
+            $data['company_id'] = $param['company_id'];
+        }
+        if (!empty($param['group_id'])) { // 角色ID
+            $data['group_id'] = $param['group_id'];
+        }
 
         if (empty($data)) {
             return show(config('code.error'), '数据不合法', '', 404);
         }
 
-        // 更新'网络忙，请重试'
+        /* 手动控制事务 s */
+        // 启动事务
+        Db::startTrans();
         try {
-            $result = model('CompanyUser')->allowField(true)->save($data, ['user_id' => $id]); // 更新
+            // 更新供应商账户
+            $res[0] = Db::name('company_user')->strict(false)->where(['user_id' => $id])->update($data);
+            $res[0] = $res[0] === false ? 0 : true;
+            //$result = model('CompanyUser')->allowField(true)->save($data, ['user_id' => $id]); // 更新
+
+            // 查询供应商是否已经绑定角色，TODO：暂时只考虑“账户-角色”一对一关系
+            $authGroupAccess = model('AuthGroupAccess')->where(['uid' => $id])->find();
+            if ($authGroupAccess) {
+                // 更新供应商账户角色
+                $res[1] = Db::name('auth_group_access')->strict(false)->where(['uid' => $id])->update(['group_id' => $data['group_id']]);
+                $res[1] = $res[1] === false ? 0 : true;
+            } else {
+                // 绑定供应商账户角色
+                $data1 = ['uid' => $id, 'group_id' => $data['group_id']];
+                $res[1] = Db::name('auth_group_access')->insert($data1);
+            }
+
+            // 任意一个表写入失败都会抛出异常，TODO：是否可以不做该判断
+            if (in_array(0, $res)) {
+                return show(config('code.error'), '更新失败', '', 403);
+            }
+
+            // 提交事务
+            Db::commit();
+            return show(config('code.success'), '更新成功', '', 201);
         } catch (\Exception $e) {
+            // 回滚事务
+            Db::rollback();
             return show(config('code.error'), '网络忙，请重试', '', 500);
         }
-        if (false === $result) {
-            return show(config('code.error'), '更新失败', '', 403);
-        } else {
-            return show(config('code.success'), '更新成功', '', 201);
-        }
+        /* 手动控制事务 e */
     }
 
     /**
