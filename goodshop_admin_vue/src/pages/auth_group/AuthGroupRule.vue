de1a7bd8ca257bfd1f7159a90145678934777d03
Greetme <1980900198@qq.com>
2020-02-20 21:46:56 +0800

    auth_group_rule

--- goodshop_admin_vue/src/pages/auth_group/AuthGroupRule.vue
+++ goodshop_admin_vue/src/pages/auth_group/AuthGroupRule.vue
@@ -11,13 +11,7 @@
 			</div>
 			<div class="">
 				<!-- Tree 树形控件（可选择层级） s -->
-				<el-tree
-					:props="props"
-					:load="loadNode"
-					lazy
-					show-checkbox
-					@check-change="handleCheckChange">
-				</el-tree>
+				<el-tree :data="data" :props="defaultProps" show-checkbox @node-click="handleNodeClick"></el-tree>
 				<!-- Tree 树形控件 e -->
 			</div>
 		</el-card>
@@ -28,49 +22,50 @@
 	export default {
 		data() {
 			return {
-				props: {
-					label: 'name',
-					children: 'zones'
-				},
-				count: 1
+				data: [{
+					label: '一级 1',
+					children: [{
+						label: '二级 1-1',
+						children: [{
+							label: '三级 1-1-1'
+						}]
+					}]
+					}, {
+						label: '一级 2',
+						children: [{
+						label: '二级 2-1',
+						children: [{
+							label: '三级 2-1-1'
+						}]
+					}, {
+						label: '二级 2-2',
+						children: [{
+							label: '三级 2-2-1'
+							}]
+						}]
+					}, {
+						label: '一级 3',
+						children: [{
+						label: '二级 3-1',
+						children: [{
+							label: '三级 3-1-1'
+						}]
+						}, {
+							label: '二级 3-2',
+							children: [{
+								label: '三级 3-2-1'
+							}]
+						}]
+				}],
+				defaultProps: {
+					children: 'children',
+					label: 'label'
+				}
 			};
 		},
 		methods: {
-			handleCheckChange(data, checked, indeterminate) {
-				console.log(data, checked, indeterminate);
-			},
 			handleNodeClick(data) {
 				console.log(data);
-			},
-			loadNode(node, resolve) {
-				if (node.level === 0) {
-					return resolve([{ name: 'region1' }, { name: 'region2' }]);
-				}
-				if (node.level > 3) return resolve([]);
-	
-				var hasChild;
-				if (node.data.name === 'region1') {
-					hasChild = true;
-				} else if (node.data.name === 'region2') {
-					hasChild = false;
-				} else {
-					hasChild = Math.random() > 0.5;
-				}
-	
-				setTimeout(() => {
-					var data;
-					if (hasChild) {
-						data = [{
-							name: 'zone' + this.count++
-						}, {
-							name: 'zone' + this.count++
-						}];
-					} else {
-						data = [];
-					}
-		
-					resolve(data);
-				}, 500);
 			}
 		}
 	};
